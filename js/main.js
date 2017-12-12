77ab16dc0c1cbfab2816120b8785b45172828ae2
Csisztai Arnold <csisztai.arnold@gmail.com>
2017-12-12 20:24:10 +0100

    Basic search in titles

--- js/main.js
+++ js/main.js
@@ -7,6 +7,30 @@ $(document).ready(function(){
         var router = new Navigo(root, useHash, hash);
 
         router
+            .on('/search/:query', function (params)  {
+                var html = '';
+                var posts = result['posts'];
+                posts.forEach(function(post) {
+                    var title = post['title'].toLowerCase();
+                    var query = params.query.toLowerCase();
+                    if(title.includes(query)) {
+                        html +=
+                            '<h3><a href="/#!/' + post['clean_url'] + '" class="post-title" id="' + post['clean_url'] +'">' +
+                            post['title'] +
+                            '<div class="datetime">' + post['datetime'] + '</div>' +
+                            '</a></h3>';
+                    }
+                });
+                $('title').empty().text('Absent Minded Alertness');
+                if(html === '') {
+                    html +=
+                        '<h3>No results :(</h3>';
+                }
+                $('#app').hide().html(html).fadeIn('fast');
+            })
+            .resolve();
+
+        router
             .on(function () {
                 var html = '';
                 var posts = result['posts'];
@@ -33,21 +57,41 @@ $(document).ready(function(){
         router
             .on('/:clean_url', function (params)  {
                 var posts = result['posts'];
+                var post = '';
                 posts.forEach(function(entry) {
                     if(params.clean_url == entry['clean_url']) {
                         post = entry;
                     }
                 });
-                var html =
-                    '<article>' +
-                    '<h3>' + post['title'] +
-                    '<div class="datetime">written on ' + post['datetime'] + '</div></h3>' +
-                    '<div class="content">' + post['content'] + '</div>' +
-                    '</article><script>$(\'pre code\').each(function(i, block) { hljs.highlightBlock(block);});</script>';
+                if(post['title'] !== undefined){
+                    var html =
+                        '<article>' +
+                        '<h3>' + post['title'] +
+                        '<div class="datetime">written on ' + post['datetime'] + '</div></h3>' +
+                        '<div class="content">' + post['content'] + '</div>' +
+                        '</article><script>$(\'pre code\').each(function(i, block) { hljs.highlightBlock(block);});</script>';
+                } else {
+                    var html =
+                        '<article>' +
+                        '<h3>No content found</h3>' +
+                        '<div class="content" style="text-align: center">The content you are looking for was either removed or renamed. Use the search or get back to the main page by clicking on the logo.</div>' +
+                        '</article>';
+                }
                 $('title').empty().text(post['title']);
                 $('#app').hide().html(html).fadeIn('fast');
             })
             .resolve();
+
+        $(document).on('keyup', '.search', function(e){
+            var query = $(this).val();
+            if(query.trim() !== '') {
+                router.navigate('/search/' + $(this).val());
+            } else {
+                router.navigate('/');
+            }
+            e.stopPropagation();
+            return false;
+        });
 
         $(document).on('click', '#home', function(e){
             router.navigate('/');
